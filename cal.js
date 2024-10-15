d803dcc79db7cdff17f4ffb1cfceb2f84ebdd46a
rexdy <jnani@duck.com>
2024-10-15 20:19:21 +0530

    added keyboard support

--- cal.js
+++ cal.js
@@ -1,11 +1,49 @@
-const display=document.getElementById("display");
+let display=document.getElementById("display");
 
 function appendTodisplay(input){
     display.value+=input;
 }
+
+document.addEventListener('keydown', function(event) {
+    event.preventDefault();
+    if (event.key === 'Enter') {
+        calculate();
+    } else if (event.key === 'Delete') {
+        cleardisplay();
+    } else if(event.key === 'Backspace'){
+        clearLast();
+    } else if(event.key === 'Space'){
+        console.log();
+    } else {
+        console.log("Key pressed: " + event.key);
+        if(event.key === '-'){
+            appendTodisplay(event.key);
+        } else if(event.key === '/'){
+            appendTodisplay(event.key);
+        } else if(event.key === '+'){
+            appendTodisplay(event.key);
+        } else if(event.key === '*'){
+            appendTodisplay(event.key);
+        }
+        else if(!isNaN(event.key)){
+            appendTodisplay(event.key);
+        }
+        else{
+            console.log("Invalid Expression");
+        }
+    }
+});
+
  function cleardisplay(){
     display.value="";
  }
+
+ function clearLast(){
+    let text = display.textContent;
+    text = text.substring(0, value.length - 1);
+    display.textContent = text;
+ }
+
  function calculate(){
     try{
     display.value=eval(display.value)
